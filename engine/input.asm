f4cecf854c5fbe2ac52c82557334f47a9c28bb6c
Mena Tawadros <mena2871@gmail.com>
2023-01-27 14:30:58 -0700

    add input frame loop

--- engine/input.asm
+++ engine/input.asm
@@ -1,8 +1,20 @@
 .include "engine/drivers/input/interface.asm"
+.define CNTRL1L $4218
+.define CNTRL1H $4219
+
+.ENUM $80
+	Joy1A	db	;B, Y, Select, Start, Up, Down, Left, Right
+	Joy1B	db	;A, X, L, R, iiii-ID
+.ENDE
 
 Input_Init:
     jsr INPUT_Init
     rts
 
 Input_Frame:
+	lda CNTRL1L     ; $4218
+	sta Joy1A
+	lda CNTRL1H     ; $4219
+	sta Joy1B
+    nop
     rts
