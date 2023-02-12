1c520551a2c08b34c29a3db4870456327b73e7f5
Justin Van Horne <justinvh@gmail.com>
2023-02-11 23:21:29 -0500

    Add build/ folder for output
    - Update build.bat and build.sh to reference new path

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-del spc700.obj top-gun.obj out\top-gun.smc out\top-gun.sym
-wla-spc700 -k -x -i -D DEBUG=1 -o spc700.obj engine\drivers\spc700\driver.asm
-wla-65816 -k -x -i -D DEBUG=1 -o top-gun.obj game\main.asm
-wlalink -i -S -A game\top-gun.link out\top-gun.smc
+del build\spc700.obj build\top-gun.obj build\top-gun.smc build\top-gun.sym
+rem wla-spc700 -k -x -i -D DEBUG=1 -o spc700.obj engine\drivers\spc700\driver.asm
+wla-65816 -v -k -x -i -D DEBUG=1 -o build\top-gun.obj game\main.asm
+wlalink -v -i -S -A game\top-gun.link build\top-gun.smc
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wla-spc700 -k -x -i -D DEBUG=1 -o spc700.obj engine/drivers/spc700/driver.asm
-wla-65816 -k -x -i -D DEBUG=1 -o top-gun.obj game/main.asm
-wlalink -i -S -A game/top-gun.link out/top-gun.smc
-cp -r out/* /mnt/c/top-gun-3
+rm build/spc700.obj build/top-gun.obj build/top-gun.smc build/top-gun.sym
+# wla-spc700 -k -x -i -D DEBUG=1 -o spc700.obj engine\drivers\spc700\driver.asm
+wla-65816 -v -k -x -i -D DEBUG=1 -o build/top-gun.obj game/main.asm
+wlalink -v -i -S -A game/top-gun.link build/top-gun.smc
+cp -r build/* /mnt/c/top-gun-3
