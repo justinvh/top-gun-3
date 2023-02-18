f118defefe1d4b1cc231be8d714e235ff9ce92da
Mena Tawadros <mena2871@gmail.com>
2023-02-18 12:15:19 -0700

    queue test

--- engine/input.asm
+++ engine/input.asm
@@ -7,14 +7,34 @@
     Joy1B db    ;A, X, L, R, iiii-ID
 .ENDE
 
+;Defining and init Queue here does not seem to be pushing/pop
+;Defining and init in Main seems to work for push, but it keeps pushing over memory
+Queue_Define("INPUT1_Q", $0090, #$0002)
+Queue_Define("INPUT2_Q", $00A0, #$0002)
+
 Input_Init:
+    A16_XY16
+    jsr INPUT1_Q_Init
+    jsr INPUT2_Q_Init
+    A8_XY16
     rts
 
 Input_Frame:
-    lda JOY1L
-    sta Joy1A
-    lda JOY1H
-    sta Joy1B
+    A16_XY16
+    ; lda JOY1L
+    lda #$FFFF
+    jsr INPUT1_Q_Push
+    ; lda JOY1H
+    jsr INPUT1_Q_Push
+    ; lda #$AAAA
+    jsr INPUT1_Q_Pop
+    jsr INPUT1_Q_Pop
+    ; sta $86
+    ; sta Joy1A
+    ; lda JOY1H
+    ; sta Joy1B
+
+    A8_XY16
     rts
 
 .ends
